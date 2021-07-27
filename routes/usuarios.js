5d04621b1efa7118d8c8927395a313443163f1a0
rodolfovenegas <rodolfovenegas.es@gmail.com>
2021-07-26 22:10:47 -0400

    Agrego rutas de consultar y agregar usuarios al controlador

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,41 +1,19 @@
 const express = require('express')
 const route = express.Router()
-const { getUsuarios, postUsuarios, putUsuarios, deleteUsuarios } = require('../controllers/usuarios')
+// const { getUsuarios, postUsuarios, putUsuarios, deleteUsuarios } = require('../controllers/usuarios')
+const usuarios = require('../controllers/usuarios')
 
-
-let usuarios = [
-  {id:1, nombre: 'Mateo'},
-  {id:2, nombre: 'León'},
-  {id:3, nombre: 'Rodofo'}
-]
 
 // Consulta endpoints desde el controlador
-route.get('/', getUsuarios)
-route.post('/', postUsuarios)
-route.put('/', putUsuarios)
-route.delete('/', deleteUsuarios)
-
-
+route.get('/', usuarios.getUsuarios)
+route.post('/', usuarios.postUsuarios)
+route.put('/', usuarios.putUsuarios)
+route.delete('/', usuarios.deleteUsuarios)
 // Consulta usuario por id
-route.get('/:id', (req, res) => {
-  let usuario = usuarios.find(user => user.id === parseInt(req.params.id))
-  if (!usuario) res.status(404).send('El usuario no fue encontrado')
-  res.send(usuario)
-})
-
+route.get('/consultarUsuario/:id', usuarios.consultarUsuario)
 // Nuevo usuario
-route.post('/', (req, res) => {
-  if (!req.body.nombre){
-    // 400 bad request
-    res.status(400).send('Debe ingresar un nombre')
-    return
-  }
-  const nuevoUsuario = {
-    id: usuarios.length + 1,
-    nombre: req.body.nombre
-  }
-  usuarios.push(nuevoUsuario)
-  res.send(usuarios)
-})
+route.post('/agregarUsuario', usuarios.agregarUsuario)
+
+
 
 module.exports = route
