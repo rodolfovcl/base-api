af7f10ffef19e225ba605746fad72c5ab992d09c
rodolfovenegas <rodolfovenegas.es@gmail.com>
2021-07-26 22:10:47 -0400

    Agrego rutas de consultar y agregar usuarios al controlador

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,12 @@
 const { response } = require('express')
 
+// Ejemplo de BD
+let arrayUsuarios = [
+  {id:1, nombre: 'Mateo'},
+  {id:2, nombre: 'León'},
+  {id:3, nombre: 'Rodofo'}
+]
+
 
 const getUsuarios = (req, res = response) => {
   res.json({
@@ -29,10 +36,37 @@ const deleteUsuarios = (req, res = response) => {
   })
 }
 
+const consultarUsuario = (req, res= response) => {
+  console.log('\n\n------>')
+  console.log('req.params.id', req.params.id)
+  let usuario = arrayUsuarios.find(user => user.id === parseInt(req.params.id))
+  if (!usuario) res.status(404).send('El usuario no fue encontrado')
+  res.json({
+    error: false,
+    usuario
+  })
+}
+
+const agregarUsuario = (req, res = response) => {
+    if (!req.body.nombre){
+      // 400 bad request
+      res.status(400).send('Debe ingresar un nombre')
+      return
+    }
+    const nuevoUsuario = {
+      id: arrayUsuarios.length + 1,
+      nombre: req.body.nombre
+    }
+    arrayUsuarios.push(nuevoUsuario)
+    res.send(arrayUsuarios)
+}
+
 
 module.exports = {
   getUsuarios,
   postUsuarios,
   putUsuarios,
-  deleteUsuarios
+  deleteUsuarios,
+  consultarUsuario,
+  agregarUsuario
 }
